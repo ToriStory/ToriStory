b9af4a2ebed4fb99092b1767302bb303dfbaa8e5
dkgusdkfk <dkgusdkfk@gmail.com>
2023-10-19 10:32:30 +0900

    init

---
+++ backend/tori/src/main/java/com/tori/ToriApplication.java
@@ -0,0 +1,13 @@
+package com.tori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToriApplication.class, args);
+    }
+
+}
