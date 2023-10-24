16dcf67f2be63da1181ac56439f49c2fa75f6552
김예빈 <yebin0825@naver.com>
2023-10-24 15:07:51 +0900

    S09P31A402-133 [FEATURE] 랜딩, 로그인 관련 페이지 배경 및 UI/UX 작업

---
+++ frontend/tori-story/src/components/atoms/background/AuthBackground.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from 'react';
+import { cls } from 'utils/cls';
+
+const AuthBackground = (props: PropsWithChildren<{ isLanding?: boolean }>) => {
+  const { isLanding, children } = props;
+  return (
+    <div
+      className={cls(
+        'w-screen h-screen relative before:block before:absolute before:w-full before:h-full before:bg-[url("/background.jpg")] before:bg-cover before:bg-no-repeat before:bg-[center_left_30%] before:sm:bg-center',
+        isLanding ? 'before:opacity-100' : ' before:opacity-60'
+      )}
+    >
+      <div className=' w-full h-full relative flex flex-col justify-center items-center '>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AuthBackground;
--- frontend/tori-story/src/pages/Auth/SignUp.tsx
+++ frontend/tori-story/src/pages/Auth/SignUp.tsx
@@ -9,6 +9,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { signInPage } from 'constants/pathname';
 import { Link } from 'react-router-dom';
+import AuthBackground from 'components/atoms/background/AuthBackground';
 
 // function Copyright(props: any) {
 //   return (
@@ -38,90 +39,93 @@ export default function SignUp() {
   };
 
   return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component='h1' variant='h5'>
-          회원가입
-        </Typography>
-        <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id='nickname'
-                label='닉네임'
-                name='nickname'
-                autoComplete='nickname'
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                type='email'
-                id='email'
-                label='이메일'
-                name='email'
-                autoComplete='email'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name='password'
-                label='비밀번호'
-                type='password'
-                id='password'
-                autoComplete='new-password'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name='confirmPassword'
-                label='비밀번호 확인'
-                type='password'
-                id='confirmPassword'
-                autoComplete='confirm-password'
-              />
-            </Grid>
-            {/* <Grid item xs={12}>
+    <AuthBackground>
+      <Container component='main' maxWidth='xs'>
+        <div className='bg-[rgba(255,255,255,1)] p-4 rounded-lg drop-shadow-2xl'>
+          <CssBaseline />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component='h1' variant='h4'>
+              회원가입
+            </Typography>
+            <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id='nickname'
+                    label='닉네임'
+                    name='nickname'
+                    autoComplete='nickname'
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    type='email'
+                    id='email'
+                    label='이메일'
+                    name='email'
+                    autoComplete='email'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name='password'
+                    label='비밀번호'
+                    type='password'
+                    id='password'
+                    autoComplete='new-password'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name='confirmPassword'
+                    label='비밀번호 확인'
+                    type='password'
+                    id='confirmPassword'
+                    autoComplete='confirm-password'
+                  />
+                </Grid>
+                {/* <Grid item xs={12}>
               <FormControlLabel
-                control={<Checkbox value='allowExtraEmails' color='primary' />}
-                label='I want to receive inspiration, marketing promotions and updates via email.'
+              control={<Checkbox value='allowExtraEmails' color='primary' />}
+              label='I want to receive inspiration, marketing promotions and updates via email.'
               />
             </Grid> */}
-          </Grid>
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{ mt: 3, mb: 2, fontWeight: 'bold', color: 'white' }}
-          >
-            회원가입
-          </Button>
-          <Grid container justifyContent='flex-end'>
-            <Grid item>
-              <Link to={signInPage.path} replace className=' underline text-orange-400 text-sm'>
-                로그인 하러 가기
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-      {/* <Copyright sx={{ mt: 5 }} /> */}
-    </Container>
+              </Grid>
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{ mt: 3, mb: 2, fontWeight: 'bold', color: 'white', fontSize: 20 }}
+              >
+                회원가입
+              </Button>
+              <Grid container justifyContent='flex-end'>
+                <Grid item>
+                  <Link to={signInPage.path} replace className=' underline text-orange-400 '>
+                    로그인 하러 가기
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          {/* <Copyright sx={{ mt: 5 }} /> */}
+        </div>
+      </Container>
+    </AuthBackground>
   );
 }
