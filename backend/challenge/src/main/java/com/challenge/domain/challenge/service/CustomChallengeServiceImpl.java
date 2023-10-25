3bebf408b4683ad04e963a7b2b94ed044ffc1b71
이지은 <jieun21124@naver.com>
2023-10-25 09:37:28 +0900

    S09P31A402-141 [FEATURE] 내 자유 도전 과제 조회 API 구현

--- backend/challenge/src/main/java/com/challenge/domain/challenge/service/CustomChallengeServiceImpl.java
+++ backend/challenge/src/main/java/com/challenge/domain/challenge/service/CustomChallengeServiceImpl.java
@@ -1,8 +1,10 @@
 package com.challenge.domain.challenge.service;
 
-import java.util.NoSuchElementException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.challenge.domain.challenge.dto.request.AddCustomReq;
+import com.challenge.domain.challenge.dto.response.FindCustomRes;
 import com.challenge.domain.challenge.entity.CustomChallenge;
 import com.challenge.domain.challenge.entity.CustomEntry;
 import com.challenge.domain.challenge.repository.CustomChallengeRepository;
@@ -27,14 +29,15 @@ public class CustomChallengeServiceImpl implements CustomChallengeService {
     private final CustomEntryRepository customEntryRepository;
 
     @Override
-    public void addCustom(AddCustomReq addCustomReq) {
+    public void addCustom(String accessToken, AddCustomReq addCustomReq) {
+        Long memberId = 1L;
 
         if (addCustomReq.getContent().length() > 20) {
             throw new ChallengeException(ErrorCode.CONTENT_LENGTH_OVER);
         }
 
         CustomChallenge savedCustomChallenge = customChallengeRepository.save(CustomChallenge.builder()
-                        .memberId(1L)
+                        .memberId(memberId)
                         .content(addCustomReq.getContent())
                         .displayFlag(addCustomReq.isDisplayFlag())
                 .build());
@@ -44,10 +47,29 @@ public class CustomChallengeServiceImpl implements CustomChallengeService {
         }
 
         customEntryRepository.save(CustomEntry.builder()
-                        .memberId(1L)
+                        .memberId(memberId)
                         .customChallenge(savedCustomChallenge)
                         .endDt(addCustomReq.getEndDt())
                 .build());
+    }
+
+    public List<FindCustomRes> findMyCustomChallenge(String accessToken) {
+        Long memberId = 1L;
+
+        List<CustomEntry> customEntryList = customEntryRepository.findAllByMemberId(memberId);
+
+        return customEntryList.stream()
+                .map(customEntry -> {
+                    return FindCustomRes.builder()
+                        .id(customEntry.getCustomEntryId())
+                        .content(customEntry.getCustomChallenge().getContent())
+                        .startDt(customEntry.getStartDt())
+                        .endDt(customEntry.getEndDt())
+                        .compFlag(customEntry.isCompFlag())
+                        .imgUrl(customEntry.getImgUrl())
+                        .build();
+                })
+                .collect(Collectors.toList());
     }
 
 }
