51a0a719538f0ddaa22b712c2b728d56ec51af75
이채정 <lcj000107@naver.com>
2023-10-26 10:55:29 +0900

    S09P31A402-98 [FEATURE] 무한 스크롤 기능 구현

--- frontend/tori-story/src/pages/TogetherChallenge/index.tsx
+++ frontend/tori-story/src/pages/TogetherChallenge/index.tsx
@@ -1,4 +1,5 @@
 import { AddButton } from 'components/atoms/iconButtons/AddButton';
+import OtherCustomChallengeList from 'components/organisms/challenge/OtherCustomChallengeList';
 import useAppNavigation from 'hooks/useAppNavigation';
 import { cls } from 'utils/cls';
 
@@ -6,14 +7,15 @@ const TogetherChallenge = () => {
   const navigation = useAppNavigation();
 
   return (
-    <div className={cls('relative h-full')}>
-      <div className='relative h-full'>
-        <div>나도 도전 화면</div>
+    <>
+      <div className='h-full w-full fixed -z-30  left-0 top-0 flex justify-center items-center bg-white opacity-80 before:w-full before:h-full before:bg-[url("/background.jpg")] before:bg-cover before:bg-no-repeat before:bg-[center_left_30%] before:sm:bg-center' />
+      <div className={cls(' h-full ')}>
+        <OtherCustomChallengeList />
+        <div className={cls('fixed bottom-16 right-4')}>
+          <AddButton onClick={navigation.navigateToTogetherChallengeCreate} size={36} />
+        </div>
       </div>
-      <div className={cls('absolute bottom-3 right-0')}>
-        <AddButton onClick={navigation.navigateToTogetherChallengeCreate} size={40} />
-      </div>
-    </div>
+    </>
   );
 };
 
