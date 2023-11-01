a0cc6038211056ddd5263702944092022216cada
이지은 <jieun21124@naver.com>
2023-11-01 15:18:46 +0900

    S09P31A402-295 [FIX] errorcode status 수정

--- backend/challenge/src/main/java/com/challenge/global/exception/ErrorCode.java
+++ backend/challenge/src/main/java/com/challenge/global/exception/ErrorCode.java
@@ -8,15 +8,15 @@ public enum ErrorCode {
 	// 자유 도전 에러
 	CONTENT_LENGTH_OVER(400, "자유 도전 과제 글자수 초과"),
 	CUSTOM_CHALLENGE_NOT_SAVED(404, "자유 도전 과제 등록 오류"),
-	RANDOM_CHALLENGE_NOT_FOUND(401, "존재하지 않는 랜덤 도전 과제"),
-	CATEGORY_NOT_FOUND(401, "존재하지 않는 인증 방식"),
+	RANDOM_CHALLENGE_NOT_FOUND(400, "존재하지 않는 랜덤 도전 과제"),
+	CATEGORY_NOT_FOUND(400, "존재하지 않는 인증 방식"),
 	CUSTOM_CHALLENGE_SEARCH_ERROR(400, "도전 과제 검색 정렬 기준 오류"),
 	CUSTOM_CHALLENGE_NOT_FOUND(404, "존재하지 않은 자유 도전 과제"),
 	CUSTOM_MEMBER_NOT_MATCH(404, "사용자의 자유 도전 과제가 아님"),
 	RANDOM_MEMBER_NOT_MATCH(404, "사용자의 랜덤 도전 과제가 아님"),
 
-	PHOTO_CHALLENGE_NOT_FOUND(401, "존재하지 않는 사진 도전 과제"),
-	PLACE_CHALLENGE_NOT_FOUND(401, "존재하지 않는 장소 도전 과제"),
+	PHOTO_CHALLENGE_NOT_FOUND(400, "존재하지 않는 사진 도전 과제"),
+	PLACE_CHALLENGE_NOT_FOUND(400, "존재하지 않는 장소 도전 과제"),
 
 	// 신고
 	ALREADY_REPORT_CHALLENGE(400, "이미 신고한 도전 과제"),
