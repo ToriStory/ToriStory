0b53b87d7164175a0d05ff0b12233aea0a6518f2
이채정 <lcj000107@naver.com>
2023-11-03 16:43:57 +0900

    fix: MyTori 주석 오류 해결

--- frontend/tori-story/src/pages/MyTori/index.tsx
+++ frontend/tori-story/src/pages/MyTori/index.tsx
@@ -5,8 +5,8 @@ const MyTori = () => {
   return (
     <div className='relative'>
       <div className='fixed -z-10 bg-[#fff3ed] inset-0 flex justify-center items-center bg-[url("/removeTree.png")] bg-center bg-no-repeat bg-cover inset-y-16 -bottom-16' />
-      // <DotoriBox />
-      // <MyToriButtons />
+      {/* <DotoriBox />
+      <MyToriButtons /> */}
     </div>
   );
 };
