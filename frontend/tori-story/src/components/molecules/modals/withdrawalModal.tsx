14bb7909fdadccd2bf92a028560801a8419cbec8
김예빈 <yebin0825@naver.com>
2023-11-03 09:01:15 +0900

    S09P31A402-239 [FEATURE] 회원탈퇴 api 연결하기

--- frontend/tori-story/src/components/molecules/modals/withdrawalModal.tsx
+++ frontend/tori-story/src/components/molecules/modals/withdrawalModal.tsx
@@ -1,9 +1,9 @@
 import { cls } from 'utils/cls';
 import ChoiceModal from './ChoiceModal';
-// import { withdrawalAPI } from 'apis/user';
-// import { toast } from 'react-toastify';
-// import { updateToast } from 'utils/toast';
-// import { signUpPage } from 'constants/pathname';
+import { withdrawalAPI } from 'apis/user';
+import { toast } from 'react-toastify';
+import { updateToast } from 'utils/toast';
+import useAppNavigation from 'hooks/useAppNavigation';
 
 interface WithdrawalModalProps {
   openModal: boolean;
@@ -11,22 +11,24 @@ interface WithdrawalModalProps {
 }
 
 export const WithdrawalModal = ({ openModal, setOpenModal }: WithdrawalModalProps) => {
+  const navigate = useAppNavigation();
+
   const handleCancelButton = () => {
     setOpenModal(false);
   };
 
   const handleWithdrawalButton = async () => {
     setOpenModal(false);
-    // const res = await withdrawalAPI();
-    // const withdrawalToastId = toast.loading('탈퇴 처리 중입니다');
-    // if (res.status === 200) {
-    //   localStorage.removeItem('accessToken');
-    //   updateToast(withdrawalToastId, '탈퇴가 완료되었습니다!', 'success', false, () =>
-    //     window.location.replace(signUpPage.path)
-    //   );
-    // } else {
-    //   updateToast(withdrawalToastId, `탈퇴에 실패했습니다`, 'error', false);
-    // }
+    const res = await withdrawalAPI();
+    const withdrawalToastId = toast.loading('탈퇴 처리 중입니다');
+    if (res.status === 200) {
+      localStorage.removeItem('accessToken');
+      updateToast(withdrawalToastId, '탈퇴가 완료되었습니다!', 'success', false, () =>
+        navigate.navigateToSignup()
+      );
+    } else {
+      updateToast(withdrawalToastId, `탈퇴에 실패했습니다`, 'error', false);
+    }
   };
 
   return (
