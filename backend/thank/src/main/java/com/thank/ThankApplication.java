bcd4da2659385877af5099e63949524496d94011
yebin0825 <yebin0825@naver.com>
2023-11-10 10:47:22 +0900

    S09P31A402-282 init: 감사일기 프로젝트 세팅

---
+++ backend/thank/src/main/java/com/thank/ThankApplication.java
@@ -0,0 +1,13 @@
+package com.thank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThankApplication.class, args);
+	}
+
+}
