98254d2687617a5c833e35c34795aff83613951a
김예빈 <yebin0825@naver.com>
2023-11-14 17:33:38 +0900

    S09P31A402-304 [FIX] 달력 아이콘으로 변경 및 마이페이지 프로필 적용

--- frontend/tori-story/src/components/organisms/userInfo/UserInfo.tsx
+++ frontend/tori-story/src/components/organisms/userInfo/UserInfo.tsx
@@ -1,31 +1,15 @@
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import Avatar from '@mui/material/Avatar';
-import { red } from '@mui/material/colors';
 import { Button, IconButton } from '@mui/material';
 import { getUserInfoAPI, signOutAPI } from 'apis/user';
 import { useNavigate } from 'react-router-dom';
 import { myToriPage, updateUserInfoPage } from 'constants/pathname';
 import { toast } from 'react-toastify';
 import useSWR from 'swr';
-import { orange400 } from 'constants/color';
 import { updateToast } from 'utils/toast';
 import { PenSquare } from 'lucide-react';
-
-// interface ExpandMoreProps extends IconButtonProps {
-//   expand: boolean;
-// }
-
-// const ExpandMore = styled((props: ExpandMoreProps) => {
-//   const { expand, ...other } = props;
-//   return <IconButton {...other} />;
-// })(({ theme, expand }) => ({
-//   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-//   marginLeft: 'auto',
-//   transition: theme.transitions.create('transform', {
-//     duration: theme.transitions.duration.shortest,
-//   }),
-// }));
+import { orange400 } from 'constants/color';
 
 const UserInfo = () => {
   const { data } = useSWR('/api/member', getUserInfoAPI);
@@ -49,15 +33,15 @@ const UserInfo = () => {
           avatar={
             <Avatar
               sx={{
-                bgcolor: red[500],
                 width: 100,
                 height: 100,
-                objectFit: 'cover',
-                borderRadius: '100%',
+                objectFit: 'contain',
+                padding: '4px',
                 borderColor: orange400,
                 borderWidth: 1,
               }}
-              src='https://i.pinimg.com/736x/4b/af/8d/4baf8ddeb7937f55a6ca9584b58b03e6.jpg'
+              variant='rounded'
+              src={data.data.imgUrl}
             />
           }
           action={
