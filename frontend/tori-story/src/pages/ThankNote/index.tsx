653bff2998f216473046334efe09ae55e42b79fc
김예빈 <yebin0825@naver.com>
2023-11-15 16:24:18 +0900

    S09P31A402-274 fix: 감사일기 관련 버그 수정

--- frontend/tori-story/src/pages/ThankNote/index.tsx
+++ frontend/tori-story/src/pages/ThankNote/index.tsx
@@ -5,11 +5,11 @@ import { useNavigate } from 'react-router-dom';
 import { createThankNotePage } from 'constants/pathname';
 import ThankNoteList from 'components/organisms/thankNote/ThankNoteList';
 import ThankQuote from 'components/organisms/thankNote/ThankQuote';
-import useSWR from 'swr';
 import { getThankNoteStatisticsAPI } from 'apis/thankNote';
 import { useAtomValue } from 'jotai';
 import { canAddThankNoteAtom } from 'stores/thankNote';
 import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
 
 const ThankNote = () => {
   const accessToken = localStorage.getItem('accessToken');
@@ -18,7 +18,10 @@ const ThankNote = () => {
   const today = new Date();
   const todayDate = dayjs(today).format('YYYY-MM-DD');
   const canAddThankNote = useAtomValue(canAddThankNoteAtom);
-  const { data } = useSWR('/thank', getThankNoteStatisticsAPI);
+  const [thankNoteStatistics, setThankNoteStatistics] = useState<{
+    totalCnt: number;
+    continueCnt: number;
+  }>({ totalCnt: 0, continueCnt: 0 });
   const handleCreateThankNoteButton = () => {
     navigate(createThankNotePage.path, {
       state: {
@@ -27,6 +30,13 @@ const ThankNote = () => {
       },
     });
   };
+  useEffect(() => {
+    const getThankNoteStatistics = async () => {
+      const res = await getThankNoteStatisticsAPI();
+      setThankNoteStatistics(res.data.data);
+    };
+    getThankNoteStatistics();
+  }, []);
 
   return (
     <>
@@ -38,8 +48,8 @@ const ThankNote = () => {
               <Label title={dayjs(today).format('YYYY년 MM월 DD일')} />
             </div>
             <div className='flex flex-col items-center justify-center text-xl'>
-              <div>연속 {data?.data.data.continueCnt}일째 작성 중!</div>
-              <div>지금까지 총 {data?.data.data.totalCnt}일 동안 감사일기를 작성했어요</div>
+              <div>연속 {thankNoteStatistics.continueCnt}일째 작성 중!</div>
+              <div>지금까지 총 {thankNoteStatistics.totalCnt}일 동안 감사일기를 작성했어요</div>
             </div>
             <Label title='오늘의 일기' />
             <ThankNoteList activeDate={todayDate} />
@@ -56,9 +66,9 @@ const ThankNote = () => {
         </>
       ) : (
         <>
-          <div className='h-full w-full fixed -z-30  left-0 top-0 flex justify-center items-center bg-white opacity-80 before:w-full before:h-full before:bg-[url("/background.jpg")] before:bg-cover before:bg-no-repeat before:bg-[center_left_30%] before:sm:bg-center before:opacity-60' />
+          <div className='h-full w-full fixed -z-30  left-0 top-0 flex justify-center items-center before:w-full before:h-full before:bg-[url("/background.jpg")] before:bg-cover before:bg-no-repeat before:bg-[center_left_30%] before:sm:bg-center before:opacity-80' />
           <div className={cls('max-h-full overflow-y-auto pb-12')}>
-            <Label title='2022/11/07' />
+            <Label title={dayjs(today).format('YYYY년 MM월 DD일')} />
             <div className='flex flex-col items-center justify-center'>
               <div>로그인 후 감사일기를 써보세요!</div>
             </div>
