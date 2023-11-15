33a6cb7f09a11320bac0b56573c357e3cd58a1af
김경륜 <kimriun27@gmail.com>
2023-11-15 12:58:44 +0900

    S09P31A402-431 [FIX] 마이토리 에셋 수정

--- frontend/tori-story/src/components/molecules/toriAssets/ToriAssetList.tsx
+++ frontend/tori-story/src/components/molecules/toriAssets/ToriAssetList.tsx
@@ -2,7 +2,7 @@ import { getToriAsset } from 'apis/toriApi';
 import { ToriAssetItem } from 'components/atoms/toriAsset/ToriAssetItem';
 import { useAtom } from 'jotai';
 import { useEffect } from 'react';
-import { dotoriCntAtom, randomCntAtom, totoriCntAtom } from 'stores/dotoriStore';
+import { dotoriCntAtom, randomCntAtom, totoriCntAtom, dailyCntAtom } from 'stores/dotoriStore';
 import Dotori from 'assets/images/Dotori.png';
 import RandomTicket from 'assets/images/RandomTicket.svg';
 import TotoriTicket from 'assets/images/TotoriTicket.svg';
@@ -16,10 +16,13 @@ export const ToriAssetList = () => {
   const [dotoriCnt, setDotoriCnt] = useAtom(dotoriCntAtom);
   const [randomCnt, setRandomCnt] = useAtom(randomCntAtom);
   const [totoriCnt, setTotoriCnt] = useAtom(totoriCntAtom);
+  const [dailyCnt, setDailyCnt] = useAtom(dailyCntAtom);
+
   const mapList: AssetMap = {
     DOTORI: setDotoriCnt,
     RANDOM_TICKET: setRandomCnt,
     TOTORI_TICKET: setTotoriCnt,
+    DAILY_TOTORI_TICKET: setDailyCnt,
   };
 
   useEffect(() => {
@@ -30,10 +33,15 @@ export const ToriAssetList = () => {
     const result = await getToriAsset();
     if (result.code === 200) {
       result.data.map((item: { assetNm: string; assetCnt: number }) => {
+        console.log(item.assetNm, item.assetCnt);
         mapList[item.assetNm](item.assetCnt);
       });
     }
   };
+
+  useEffect(() => {
+    console.log('에셋 받음', dotoriCnt, randomCnt, totoriCnt);
+  }, [dailyCnt, dotoriCnt, randomCnt, totoriCnt]);
 
   return (
     <div className={cls('absolute top-0 left-0 flex flex-col gap-1')}>
--- frontend/tori-story/src/stores/dotoriStore.ts
+++ frontend/tori-story/src/stores/dotoriStore.ts
@@ -4,4 +4,5 @@ import DefaultSquirrel from 'assets/images/DefaultSquirrel.png';
 export const dotoriCntAtom = atom<number>(0);
 export const randomCntAtom = atom<number>(0);
 export const totoriCntAtom = atom<number>(0);
+export const dailyCntAtom = atom<number>(0);
 export const profileToriImgUrlAtom = atom<string>(DefaultSquirrel);
