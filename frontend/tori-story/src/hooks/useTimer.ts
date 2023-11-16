abcbd0a4392b19e17c2cb5bd57727ced0990e81a
김예빈 <yebin0825@naver.com>
2023-11-16 16:20:18 +0900

    S09P31A402-434 [FIX] 변경된 api 적용 및 타이머 이슈 해결

--- frontend/tori-story/src/hooks/useTimer.ts
+++ frontend/tori-story/src/hooks/useTimer.ts
@@ -15,17 +15,22 @@ const use1Second = interval(1000);
 export const useTimer = ({ initialSeconds = 300, initiallyRunning = false } = {}) => {
   const [seconds, setSeconds] = useState(initialSeconds);
   const [running, setRunning] = useState(initiallyRunning);
-  const tick = useCallback(
-    () => (running ? (seconds > 0 ? setSeconds((seconds) => seconds - 1) : stop()) : undefined),
-    [running]
-  );
   const start = () => setRunning(true);
   const pause = () => setRunning(false);
   const reset = () => setSeconds(300);
   const stop = () => {
     pause();
-    reset();
+    // reset();
   };
+  const tick = useCallback(
+    () =>
+      running
+        ? seconds > 0
+          ? setSeconds((seconds) => seconds - 1)
+          : setRunning(false)
+        : undefined,
+    [running, seconds]
+  );
 
   use1Second(tick);
 
