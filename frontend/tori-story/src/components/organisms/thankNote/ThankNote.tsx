8eeaa44f8a6685d8f46e33de4101757d68f4a434
김경륜 <kimriun27@gmail.com>
2023-11-21 15:30:00 +0900

    S09P31A402-452 [REFACTOR] 감사일기 css 수정

--- frontend/tori-story/src/components/organisms/thankNote/ThankNote.tsx
+++ frontend/tori-story/src/components/organisms/thankNote/ThankNote.tsx
@@ -1,11 +1,29 @@
+import { cls } from 'utils/cls';
+
 interface ThankNoteProps {
   content: string;
+  hasName?: boolean;
 }
 
-const ThankNoteCard = ({ content }: ThankNoteProps) => {
+const ThankNoteCard = ({ content, hasName = false }: ThankNoteProps) => {
+  const contents = content.split(' - ');
+
   return (
-    <div className=' bg-white relative opacity-95 border-orange-400 border-2 rounded-xl px-4 py-2 my-4'>
-      <div className=' flex justify-center items-center text-2xl my-2'>{content}</div>
+    <div
+      className={cls(
+        'p-4 rounded-xl bg-white opacity-95 border-2 border-orange-400 text-lg text-orange-900'
+      )}
+    >
+      {hasName ? (
+        <>
+          <div className={cls('flex justify-center items-center text-xl')}>{contents[0]}</div>
+          <div className={cls('mt-1 flex justify-end items-center text-xl')}>- {contents[1]} -</div>
+        </>
+      ) : (
+        <>
+          <div className={cls('flex justify-center items-center text-xl')}>{content}</div>
+        </>
+      )}
     </div>
   );
 };
--- frontend/tori-story/src/pages/ThankNote/index.tsx
+++ frontend/tori-story/src/pages/ThankNote/index.tsx
@@ -2,7 +2,7 @@ import Label from 'components/atoms/challenge/Label';
 import { cls } from 'utils/cls';
 import { AddButton } from 'components/atoms/iconButtons/AddButton';
 import { useNavigate } from 'react-router-dom';
-import { createThankNotePage } from 'constants/pathname';
+import { createThankNotePage, signInPage } from 'constants/pathname';
 import ThankNoteList from 'components/organisms/thankNote/ThankNoteList';
 import ThankQuote from 'components/organisms/thankNote/ThankQuote';
 import { getThankNoteStatisticsAPI } from 'apis/thankNote';
@@ -10,10 +10,12 @@ import { useAtomValue } from 'jotai';
 import { canAddThankNoteAtom } from 'stores/thankNote';
 import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
+import { LoginModal } from 'components/molecules/modals/LoginModal';
 
 const ThankNote = () => {
   const accessToken = localStorage.getItem('accessToken');
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const today = new Date();
   const todayDate = dayjs(today).format('YYYY-MM-DD');
@@ -22,7 +24,13 @@ const ThankNote = () => {
     totalCnt: number;
     continueCnt: number;
   }>({ totalCnt: 0, continueCnt: 0 });
+
   const handleCreateThankNoteButton = () => {
+    if (!accessToken) {
+      setIsModalOpen(true);
+      return;
+    }
+
     navigate(createThankNotePage.path, {
       state: {
         content: '',
@@ -30,6 +38,7 @@ const ThankNote = () => {
       },
     });
   };
+
   useEffect(() => {
     const getThankNoteStatistics = async () => {
       const res = await getThankNoteStatisticsAPI();
@@ -40,11 +49,11 @@ const ThankNote = () => {
 
   return (
     <>
-      {accessToken ? (
-        <>
-          <div className='h-full w-full fixed -z-30  left-0 top-0 flex justify-center items-center before:w-full before:h-full before:bg-[url("/background.jpg")] before:bg-cover before:bg-no-repeat before:bg-[center_left_30%] before:sm:bg-center before:opacity-80' />
-          <div className={cls('max-h-full overflow-y-auto')}>
-            <Label title={dayjs(today).format('YYYY년 MM월 DD일')} />
+      <div className='h-full w-full fixed -z-30  left-0 top-0 flex justify-center items-center before:w-full before:h-full before:bg-[url("/background.jpg")] before:bg-cover before:bg-no-repeat before:bg-[center_left_30%] before:sm:bg-center before:opacity-80' />
+      <div className={cls('max-h-full overflow-y-auto')}>
+        <Label title={dayjs(today).format('YYYY-MM-DD')} />
+        {accessToken ? (
+          <>
             <div className='py-4 flex flex-col items-center justify-center bg-[url("/woodBoard.png")]  bg-cover bg-no-repeat rounded-xl border-2 border-orange-800 text-lg text-orange-900'>
               {thankNoteStatistics.continueCnt > 0 && (
                 <div>연속 {thankNoteStatistics.continueCnt}일째 작성 중!</div>
@@ -55,29 +64,25 @@ const ThankNote = () => {
             <Label title='오늘의 일기' />
             <ThankNoteList activeDate={todayDate} />
             <br />
-
-            {canAddThankNote && (
-              <div className={cls('fixed w-full inset-x-0 bottom-16 z-20')}>
-                <div className={cls('flex justify-center my-1')}>
-                  <AddButton title='일기 작성' onClick={() => handleCreateThankNoteButton()} />
-                </div>
-              </div>
-            )}
-          </div>
-        </>
-      ) : (
-        <>
-          <div className='h-full w-full fixed -z-30  left-0 top-0 flex justify-center items-center before:w-full before:h-full before:bg-[url("/background.jpg")] before:bg-cover before:bg-no-repeat before:bg-[center_left_30%] before:sm:bg-center before:opacity-80' />
-          <div className={cls('max-h-full overflow-y-auto pb-12')}>
-            <Label title={dayjs(today).format('YYYY년 MM월 DD일')} />
-            <div className='flex flex-col items-center justify-center'>
+          </>
+        ) : (
+          <>
+            <div className='p-4 flex flex-col items-center justify-center  bg-white relative opacity-80 border-orange-400 border-2 rounded-xl text-lg'>
               <div>로그인 후 감사일기를 써보세요!</div>
             </div>
             <Label title='감사 문구' />
             <ThankQuote />
+          </>
+        )}
+        {canAddThankNote && (
+          <div className={cls('fixed w-full inset-x-0 bottom-16 z-20')}>
+            <div className={cls('flex justify-center my-1')}>
+              <AddButton title='일기 작성' onClick={() => handleCreateThankNoteButton()} />
+            </div>
           </div>
-        </>
-      )}
+        )}
+        <LoginModal openModal={isModalOpen} setOpenModal={setIsModalOpen} />
+      </div>
     </>
   );
 };
